f23d36ad63ab36622856a1c104423349f39e7af5
Haoyu Xiong <haoyuxiongac@outlook.com>
2017-03-03 20:55:03 -0500

    hw01, part1, start-up

---
+++ src/Assignment01/ANN.java
@@ -0,0 +1,22 @@
+/* *****************************************
+  * CSCI205 - Software Engineering and Design
+  * Spring 2017 *
+  * Name: Haoyu Xiong and Jingya Wu
+  * Date: Mar 3, 2017
+  * Time: 8:53:25 PM *
+  * Project: csci205_hw
+  * Package: Assignment01
+  * File: ANN
+  * Description:
+  *
+  * ****************************************
+ */
+package Assignment01;
+
+/**
+ *
+ * @author haoyuxiong
+ */
+public class ANN {
+
+}
---
+++ src/Assignment01/ANN_Client.java
@@ -0,0 +1,22 @@
+/* *****************************************
+  * CSCI205 - Software Engineering and Design
+  * Spring 2017 *
+  * Name: NAMES of team members
+  * Date: Mar 3, 2017
+  * Time: 8:54:17 PM *
+  * Project: csci205_hw
+  * Package: Assignment01
+  * File: ANN_Client
+  * Description:
+  *
+  * ****************************************
+ */
+package Assignment01;
+
+/**
+ *
+ * @author haoyuxiong
+ */
+public class ANN_Client {
+
+}
