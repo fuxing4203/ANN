b586069c165db2f43f863c5e192b906e2312801b
Haoyu Xiong <haoyuxiongac@outlook.com>
2017-03-03 23:20:15 -0500

    hw01, part1, ANN and Perceptron class Complete

--- src/Assignment01/ANN.java
+++ src/Assignment01/ANN.java
@@ -13,10 +13,87 @@
  */
 package Assignment01;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author haoyuxiong
  */
 public class ANN {
+
+    private ArrayList<Perceptron> perceptronsList;
+
+    private int layer;
+
+    private int numIp;
+
+    public ANN(int numInp, int numOut, int layer) {
+
+        this.layer = layer;
+
+        this.numIp = numInp;
+
+        this.perceptronsList = new ArrayList<Perceptron>();
+        for (int i = 0; i < numOut; i++) {
+            this.perceptronsList.add(new Perceptron(numInp));
+
+        }
+    }
+
+    public int getNumIp() {
+        return numIp;
+    }
+
+    public void setNumIp(int numIp) {
+        this.numIp = numIp;
+    }
+
+    public int getLayer() {
+        return layer;
+    }
+
+    public void setLayer(int layer) {
+        this.layer = layer;
+    }
+
+    public int getNumOfOutput() {
+        return this.perceptronsList.size();
+    }
+
+    public ArrayList<Perceptron> getPerceptronsList() {
+        return perceptronsList;
+    }
+
+    public void setPerceptronsList(ArrayList<Perceptron> perceptronsList) {
+        this.perceptronsList = perceptronsList;
+    }
+
+    public void train_ANN(double[][] data) {
+        int row = data.length;
+        int col = data[0].length;
+
+        for (int i = 0; i < row; i++) {
+
+            for (int j = this.numIp; j < col; j++) {
+                this.perceptronsList.get(j + 1 - col).train_Perceptron(data[i],
+                                                                       j);
+            }
+
+        }
+
+    }
+
+    public double[][] classify_ANN(double[][] classifyData) {
+        int row = classifyData.length;
+        double outputResult[][] = new double[row][this.perceptronsList.size()];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < this.perceptronsList.size(); j++) {
+                outputResult[i][j] = this.perceptronsList.get(j).classify_Perceptron(
+                        classifyData[i]);
+            }
+        }
+        return outputResult;
+
+    }
 
 }
---
+++ src/Assignment01/Perceptron.java
@@ -0,0 +1,96 @@
+/* *****************************************
+  * CSCI205 - Software Engineering and Design
+  * Spring 2017 *
+  * Name: NAMES of team members
+  * Date: Mar 3, 2017
+  * Time: 9:31:02 PM *
+  * Project: csci205_hw
+  * Package: Assignment01
+  * File: Perceptron
+  * Description:
+  *
+  * ****************************************
+ */
+package Assignment01;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author haoyuxiong
+ */
+public class Perceptron {
+
+    private ArrayList<Double> weights;
+    final private static double alpha = 0.3;
+
+    /**
+     *
+     * @param numInp
+     */
+    public Perceptron(int numInp) {
+
+        this.weights = new ArrayList<Double>();
+        Random randomDoubleGenerator = new Random();
+        for (int i = 0; i < numInp + 1; i++) {
+            this.weights.add(randomDoubleGenerator.nextDouble() - 0.5);
+
+        }
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Double> getWeights() {
+        return weights;
+    }
+
+    /**
+     *
+     * @param weights
+     */
+    public void setWeights(ArrayList<Double> weights) {
+        this.weights = weights;
+    }
+
+    public void train_Perceptron(double[] data1,
+                                 int indexOfOutput) {
+        double net = 1 * this.weights.get(0);
+        for (int i = 0; i < this.weights.size() - 1; i++) {
+            net += data1[i] * this.weights.get(i + 1);
+
+        }
+        double Error = data1[indexOfOutput] - step_function(net);
+        this.weights.set(0, this.weights.get(0) + alpha * 1 * Error);
+        for (int j = 1; j < this.weights.size(); j++) {
+            this.weights.set(j,
+                             this.weights.get(j) + alpha * data1[j - 1] * Error);
+
+        }
+
+    }
+
+    public double classify_Perceptron(double[] classify_Data1) {
+        double net = 1 * this.weights.get(0);
+        for (int i = 0; i < this.weights.size() - 1; i++) {
+            net += classify_Data1[i] * this.weights.get(i + 1);
+
+        }
+        return step_function(net);
+
+    }
+
+    public double step_function(double net) {
+        if (net >= 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+
+    }
+
+}
